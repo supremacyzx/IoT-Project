277750667fc8062772cafef2dec23589fe8eda70
Naschkater <furryalpacca@gmail.com>
2025-04-24 11:24:00 +0200

    Add basic RFID Auth | Refactor & Cleanup

--- microcontroller/Main/Main.ino
+++ microcontroller/Main/Main.ino
@@ -1,4 +1,9 @@
-//V1.0.1
+// Title:   Main Arduino Code Smarte Gebaeudeueberwachung
+// Author:  DD
+// Version: 1.0.2
+
+#pragma region :: Lib Imports
+
 
 #include <ArduinoMqttClient.h>
 #include <WiFi.h>
@@ -11,13 +16,25 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#pragma endregion
 
-const char broker[] = "10.93.136.51";
-int        port     = 1883;
-const char topic[]  = "/home/dreh";
+#pragma region :: Constants / Definitions / Config
+
+//Helper Vars
+bool armed = true;
+String access_ids[] = {"76759f30"};
+int num_access_ids = sizeof(access_ids) / sizeof(access_ids[0]); // Calculate the number of access IDs
+
+
+//Wifi Conf
 char ssid[] = "FES-SuS";  // Enter your Wi-Fi SSID here
 char password[] = "SuS-WLAN!Key24";  // Enter your Wi-Fi password here
 bool useWifi = false;
+WiFiClient wifiClient;  // Wi-Fi client
+
+//LED Setup
+const int LEDG = 22;      //Initialisiert die Integer-Variable LED konstant auf den Wert 23, die LED soll an den GPIO-Pin 23 angeschlossen werden
+const int LEDR = 26; 
 
 //RFID Setup
 MFRC522DriverPinSimple ss_pin(13);
@@ -30,88 +47,23 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define SDA_PIN 15
 #define SCL_PIN 14
 
+// MQTT Conf
+const char broker[] = "10.93.136.51";
+int        port     = 1883;
+MqttClient mqttClient(wifiClient);  // Initialize MQTT client with Wi-Fi client
+
 // DHT11 Setup
 #define DHTPIN 25       // Pin connected to the DHT11 sensor
 #define DHTTYPE DHT11   // DHT 11 type sensor
-
-
-WiFiClient wifiClient;  // Wi-Fi client
-MqttClient mqttClient(wifiClient);  // Initialize MQTT client with Wi-Fi client
 DHT dht(DHTPIN, DHTTYPE);  // Initialize DHT sensor
 
-// Define a struct to hold the temperature and humidity values
-struct SensorData {
-  float temperature;
-  float humidity;
-};
-
-SensorData readDHTSensor() {
-  SensorData data;
-
-  data.humidity = dht.readHumidity();
-  data.temperature = dht.readTemperature();  
-
-  if (isnan(data.humidity) || isnan(data.temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-    data.temperature = NAN;  
-    data.humidity = NAN;
-  }
-
-  return data;
-}
-
-
-void setup() {
-  Serial.begin(9600);
-  Wire.begin(SDA_PIN, SCL_PIN);
-  
-  lcd.init();                      // Initialize the LCD
-  lcd.backlight();                 
-
-  mfrc522.PCD_Init();    // Init MFRC522 board.
-  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
-
-  if (useWifi) {
-    connectToWifi(ssid, password);
-   // connectToMQTT(broker, port);
-  }
-
-  dht.begin();
-
-
-}
 
 
 
+#pragma endregion
 
-void loop() {
+#pragma region :: Custom Methods
 
-  
-  SensorData sensorData = readDHTSensor();
-
-  publishMessage("RZ/Temperatur", String(sensorData.temperature));
-  publishMessage("RZ/Feuchtigkeit", String(sensorData.humidity));
-  lcd.setCursor(0, 0);             // Set cursor to column 0, row 0
-  lcd.print("Tmp: " + String(sensorData.temperature) + "C");
-  lcd.setCursor(0,1);
-  lcd.print("LF: " + String(sensorData.humidity) + "%");
-  delay(1000);
-  lcd.clear();
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-      return;
-    }
-
-  // Select one of the cards.
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-  readCard();
-  Serial.println("Checking for card");
- 
-
-  
-
-}
 
 String readCard(){
   MFRC522Debug::PrintUID(Serial, (mfrc522.uid));
@@ -127,6 +79,18 @@ String readCard(){
   }
   Serial.println(uidString);
   return uidString;
+}
+
+bool checkAccess(String id){
+  Serial.println(id);
+  for(int i = 0; i < num_access_ids; i++){
+    if(access_ids[i] == id){
+      Serial.println("Access granted");
+      return true; // ID found in the access list
+    }
+  }
+  Serial.println("Access denied");
+  return false; // ID not found
 }
 
 void connectToMQTT(const char* broker, const int port){
@@ -164,3 +128,95 @@ void publishMessage(String topic, String msg) {
   mqttClient.endMessage();         // End the message and send it
   Serial.println("Sent MQTT msg to " + topic + " Value: " + msg);
 }
+
+
+struct SensorData {
+  float temperature;
+  float humidity;
+};
+
+SensorData readDHTSensor() {
+  SensorData data;
+
+  data.humidity = dht.readHumidity();
+  data.temperature = dht.readTemperature();  
+
+  if (isnan(data.humidity) || isnan(data.temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    data.temperature = NAN;  
+    data.humidity = NAN;
+  }
+
+  return data;
+}
+
+#pragma endregion
+
+#pragma region :: Main Methods Setup & Loop
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  
+  pinMode(LEDG, OUTPUT);          
+  pinMode(LEDR, OUTPUT);  
+ 
+
+  lcd.init();                      // Initialize the LCD
+  lcd.backlight();                 
+
+  mfrc522.PCD_Init();    // Init MFRC522 board.
+  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
+
+  if (useWifi) {
+    connectToWifi(ssid, password);
+   // connectToMQTT(broker, port);
+  }
+
+  dht.begin();
+
+
+}
+
+
+void loop() {
+
+  SensorData sensorData = readDHTSensor();
+  publishMessage("RZ/Temperatur", String(sensorData.temperature));
+  publishMessage("RZ/Feuchtigkeit", String(sensorData.humidity));
+  lcd.setCursor(0, 0);             // Set cursor to column 0, row 0
+  lcd.print("Tmp: " + String(sensorData.temperature) + "C");
+  lcd.setCursor(0,1);
+  lcd.print("LF: " + String(sensorData.humidity) + "%");
+
+  if (armed){
+    digitalWrite(LEDG, LOW);
+    digitalWrite(LEDR, HIGH);
+  }else{
+    digitalWrite(LEDR, LOW);
+    digitalWrite(LEDG, HIGH);
+  }
+  
+  
+  delay(1000);
+  lcd.clear();
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+      return;
+    }
+
+  // Select one of the cards.
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  String cardID = readCard();
+  if (armed and cardID){
+    checkAccess(cardID);
+  }
+  
+ 
+
+  
+
+}
+
+#pragma endregion
