bd5af8f4617b3ed2a7c849a19af9714302afd497
Yannick Rörsch <supremacy@airshade.net>
2025-05-06 09:51:01 +0200

    starting with incident / logs page

---
+++ frontend/src/app/pages/incidents/incidents.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-incidents',
+  imports: [LucideAngularModule],
+  templateUrl: './incidents.component.html',
+  styleUrl: './incidents.component.scss'
+})
+
+export class IncidentsComponent {
+
+}
