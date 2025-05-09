d123f5f908d3ae1d725cfea954bbc8a20fbebdd2
Yannick Rörsch <supremacy@airshade.net>
2025-05-09 16:01:24 +0200

    Removed landing mockup and added actual component.

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -37,39 +37,20 @@ export class LandingComponent {
       link: 'https://flask.palletsprojects.com/'
     },
     {
-      image: 'images/go.svg',
-      link: 'https://golang.org/'
-    },
-    {
       image: 'images/docker.svg',
       link: 'https://www.docker.com/'
     },
     {
       image: 'images/sqlite.svg',
       link: 'https://sqlite.org/'
-    }
-  ];
-
-  hardwareStack: SlideContentModel[] = [
+    },
     {
       image: 'images/proxmox.svg',
       link: 'https://www.proxmox.com/'
     },
     {
-      image: 'images/hetzner.svg',
-      link: 'https://www.hetzner.com/'
-    },
-    {
-      image: 'images/cloudflare.svg',
-      link: 'https://www.cloudflare.com/'
-    },
-    {
       image: 'images/mqtt.svg',
       link: 'https://mqtt.org/'
-    },
-    {
-      image: 'images/zigbee.svg',
-      link: 'https://zigbeealliance.org/'
     }
   ];
 
@@ -79,4 +60,3 @@ export class LandingComponent {
     this.router.navigate([path]);
   }
 }
-
